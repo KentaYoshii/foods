47f033c5a52ecc0bfe269de49562a2f4ef3a886a
Kenta Yoshii <kenta_yoshii@brown.edu>
2023-06-22 08:00:08 +0900

    add about page

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material";
 const theme = createTheme();
 
 theme.typography.h2 = {
-    fontSize: '2.5rem',
+    fontSize: '2.4rem',
     '@media (min-width:600px)': {
         fontSize: '3.0rem',
     },
@@ -13,7 +13,9 @@ theme.typography.h2 = {
 }
 
 theme.typography.body1 = {
-    fontSize: '1.0rem',
+    fontSize: '1.05rem',
+    fontWeight: 300,
+    lineHeight: 1.25,
     '@media (min-width:600px)': {
         fontSize: '1.25rem',
     },
